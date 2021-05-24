c1ea71e0c989af3c62ef4d01232db44157ca17ef
Armando Leonardo Garcia Zambrano <legazagz@outlook.com>
2021-05-24 16:47:05 -0500

    ya no hice nada XD

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
-import { SideBarComponent } from './side-bar/side-bar.component';
-
 
 
 @NgModule({
   declarations: [
     MainDashboardComponent,
-    SideBarComponent
   ],
   imports: [
     CommonModule
